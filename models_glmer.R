dd790a81bf451427725e8f2a16982f535dca36af
Violeta Calian <45428243+violetacln@users.noreply.github.com>
2021-10-27 14:02:23 +0000

    Create models_glmer.R

---
+++ models_glmer.R
@@ -0,0 +1,3 @@
+### using glmer() of lme4-package and frequentist estimates of multilevel models for individual/aggregated events
+### results are useful because fast but also because may be used to speed up convergence of Bayesian methods
+### to be updated 
