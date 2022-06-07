d0bda99dfd62b55c7884c1591cf0dabf916c8530
Violeta Calian <45428243+violetacln@users.noreply.github.com>
2022-06-07 14:03:24 +0000

    Update and rename models_brms.R to models_Bayesian_hierarchical.R

---
+++ models_Bayesian_hierarchical.R
@@ -0,0 +1,125 @@
+### Bayesian hierarchical models for individual/aggregated events (births, deaths, mogration)
+
+library(brms)
+
+################# M1. Models for counts and rates data ############################
+
+### given: data frames (dbirths, ddeaths, dmigr) of counts of events (nr) and counts of exposed (nrExposed), 
+### by municipality (svf), age (aldur), time (ar), gender (kyn) , type of migration (gerd)
+
+## we use poisson models with offset terms---------
+## brm(y ~ ... + offset(log(n)), family = poisson())
+
+## Note: testing: t2(ar, aldur) and t2(ar)+t2(aldur) ## **********************
+
+mb <- brms::brm(nr ~ t2(ar) + t2(aldur) + (1|svf) + citiz + offset(log(nrExposed)), 
+                family=poisson(), data= dbirths,
+                warmup = 500, iter = 2000, chains = 2, seed=123)
+
+# mb1 <- brms::brm(nr ~ t2(ar,aldur) + (1|svf) + citiz + offset(log(nrExposed)), 
+#                  family=poisson(), data= dbirths,
+#                  warmup = 500, iter = 2000, chains = 2, seed=123)
+
+
+md <- brms::brm(nr ~ t2(ar) + t2(aldur)
+                  # t2(ar, aldur) 
+                + kyn + offset(log(nrExposed)),  
+                family=poisson(), data= ddeaths, 
+                warmup = 500, iter = 2000, chains = 2, seed=123)
+
+mm <- brms::brm(nr ~ t2(aldur) + t2(ar) 
+                # t2(ar, aldur)
+                + kyn + gerd + citiz 
+                 + (1|svf) 
+                + offset(log(nrExposed)) ,  
+                family=poisson(), data= dmigr,
+                warmup = 500, iter = 2000, chains = 2, seed=123)
+
+
+#------------ model performance evaluation -------------------
+
+# chose the model to evaluate
+mm <- md ## or any other  
+summary(mm)
+performance::variance_decomposition(mm)
+plot(mm)
+brms::ranef(mm)
+brms::conditional_effects(mm, points=TRUE)
+brms::conditional_effects(mm, surface=TRUE)
+# brms::conditional_effects(mm, points=TRUE, re_formula=NULL)  ## much larger bands, when RE considered
+
+brms::pp_check(mm, type = "stat_2d")
+#pp_check(mm, type = "error_hist", ndraws = 20)
+pp_check(mm, type = "scatter_avg", ndraws = 100)
+#pp_check(mm, type = "rootogram")
+pp_check(mm, type = "loo_pit")
+brms::bayes_R2(mm)
+#------------------------------------------------------------------------------------
+
+
+
+
+################# M2. Models for microdata #############################
+
+## given a data frame called micro, containing all microdata, with y, yd, y_immExt, y_em_ext, y_m_int:
+## binary valued responses concerning
+## births, deaths, immigration, emigration, internal migration
+## other variables are:
+## svf-municipality, Menntun-education, fjs-size of family, svffjolgun-size change of municipality, kyn-gender, 
+## aldur-age, aldurF-age as factor, fjolskyldustaerd-size of family, rfang-citizenship, fland-country of birth 
+
+# Bayesian "null" model, for y=binary response corresponding to birth events ------------------------
+library(brms)
+m_bayes <- brms::brm(formula= y ~ 1 + (1|svf)
+                       ,data=micro
+                     ,family = binomial()
+                     ,warmup = 500, iter = 2000, chains = 2,inits= "0",
+                     seed = 123)
+
+## NOTE: may use the future package for more flexible parallelization
+# library(future)
+# plan(multiprocess)
+# fit7 <- update(fit7, future = TRUE)
+
+# evaluate the model --------------------------
+summary(m_bayes)
+plot(m_bayes)
+## head(predict(m_bayes))
+# plot(brms::conditional_effects(m_bayes), ask = FALSE)
+brms::loo(m_bayes)
+brms::pp_check(m_bayes)
+
+# Bayesian model for births, with age and time as RE-------------------------------
+library(brms)
+m1_bayes <- brms::brm(formula= y|trials(1) ~ 1 +
+                      (1|aldurF)
+                    +  (1|arF) 
+                  # + (1|svf)
+                     ,data=micro
+                     ,family = binomial()
+                     ,warmup = 500, iter = 2000, chains = 2,inits= "0",
+                     #cores=2,
+                     seed = 123)
+
+
+# Bayesian model for births, using Gaussian processes by (age, time) -------------------------------------------------------------
+m1gp_bayes <- brms::brm(formula= y|trials(1)~ 1 + gp(ar, aldur)  ##, by=Menntun)
+                      ##  (1|svf)
+                      ,data=micro
+                      ,family = binomial()
+                      ,warmup = 500, iter = 2000, chains = 1,inits= "0",
+                      #cores=2,
+                      seed = 123)
+# evaluate the model ---------------
+summary(m1gp_bayes)
+plot(m1gp_bayes)
+##head(predict(m_bayes))
+# plot(brms::conditional_effects(m_bayes), ask = FALSE)
+#brms::loo(m1gp_bayes)
+#brms::pp_check(m1gp_bayes)
+
+### same types of models are tested for yd, y_immExt, y_em_ext, y_m_int responses ----------------
+### ----------------------------------------------------------------------------------------------
+
+
+
--- models_brms.R
+++
@@ -1,2 +0,0 @@
-### Bayesian hierarchical/multilevel models for individual/aggregated events (births, deaths, mogration of various types)
-#### to be updated 
