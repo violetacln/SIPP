69306be050fc5f7fdfe2694771a1f5ae9cbddda2
Violeta Calian <45428243+violetacln@users.noreply.github.com>
2022-06-07 13:52:19 +0000

    Create predict_evaluate.R

---
+++ predict_evaluate.R
@@ -0,0 +1,11 @@
+
+### to be updated:
+
+#------------------------newdata sets for predictions--------------------------------
+
+
+
+#----------------------------evaluate forecasts--------------------------------------
+
+
+#-----------------population counts by (...) from start and rates b,m,d --------------
