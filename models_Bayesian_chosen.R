b9291c751df6c071cac7af968db8df4af205ee37
Violeta Calian <45428243+violetacln@users.noreply.github.com>
2022-12-06 11:18:21 +0000

    Create models_Bayesian_chosen.R
    placeholder for the final models and population predictions

---
+++ models_Bayesian_chosen.R
@@ -0,0 +1,13 @@
+## the Bayesian, hierarchical models with best performance----------
+
+## births --------------------
+
+## deaths ---------------------
+
+## migration ------------------
+
+## stochastic ccmp (cohort component method for projecting the population numbers) ------------
+
+## summary results ------------------------
+
+## to be updated soon! --------------------
